229f452341e5b9d4a699e13cfb5e08860922c871
Nekmo <contacto@nekmo.com>
2016-11-20 19:55:53 +0100

    Search fields.

--- polls/admin.py
+++ polls/admin.py
@@ -11,6 +11,7 @@ class ChoiceInline(admin.TabularInline):
 
 class QuestionAdmin(admin.ModelAdmin):
     list_filter = ['pub_date']
+    search_fields = ['question_text']
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     fieldsets = [
         (None,               {'fields': ['question_text']}),
