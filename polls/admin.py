9f04837883148520ba617a7ee53091ac040aaecb
Nekmo <contacto@nekmo.com>
2016-11-20 19:41:57 +0100

    Question admin fields.

--- polls/admin.py
+++ polls/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
