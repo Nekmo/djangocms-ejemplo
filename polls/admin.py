28b110435032680f6233f862b76662f2828e8813
Nekmo <contacto@nekmo.com>
2016-11-20 19:53:49 +0100

    list displays and model method options.

--- polls/admin.py
+++ polls/admin.py
@@ -10,6 +10,8 @@ class ChoiceInline(admin.TabularInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    list_filter = ['pub_date']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
