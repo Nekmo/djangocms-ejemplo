56c810640e6f9b3d54fed58544ae20222dbc696c
Nekmo <contacto@nekmo.com>
2016-11-20 19:09:26 +0100

    Changed wrong position of method in model.

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,5 @@
 import datetime
-from time import timezone
+from django.utils import timezone
 
 from django.db import models
 
@@ -10,6 +10,9 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
     def __str__(self):
         return self.question_text
 
@@ -18,9 +21,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
     def __str__(self):
         return self.choice_text
